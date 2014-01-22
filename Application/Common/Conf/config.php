a9ad8b2ff24238025fbc6bd4e4c2e303b7c25e3d
jason <znlswd@gmail.com>
2014-01-22 23:08:12 +0800

    env try to sync

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'LOCAL_ENV' => array(
-		'MOPAAS_MYSQL_HOST' => 'localhost',
-		'MOPAAS_MYSQL_PORT' => '3306',
-		'MOPAAS_MYSQL_NAME' => 'blog_editor',
-		'MOPAAS_MYSQL_USERNAME' => 'root',
-		'MOPAAS_MYSQL_PASSWORD' => 'dd'	
+		'MOPAAS_MYSQL6757_HOST' => 'localhost',
+		'MOPAAS_MYSQL6757_PORT' => '3306',
+		'MOPAAS_MYSQL6757_NAME' => 'blog_editor',
+		'MOPAAS_MYSQL6757_USERNAME' => 'root',
+		'MOPAAS_MYSQL6757_PASSWORD' => 'dd'	
 		)
 );
